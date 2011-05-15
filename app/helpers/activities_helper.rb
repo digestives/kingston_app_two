44c198cdb8c0e87a10e3b3d3e3a53e240fcbdbd4
Patrick Magee <patrick.magee@live.co.uk>
2011-05-15 22:48:59 +0100

    ajax with color changes on bookings ammount - red/orange/green, not sure if this is best way and probably isnt, but untill something else this is good :)

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,14 @@
 module ActivitiesHelper
+
+   def display_color(current, maximum)
+      if current >= maximum
+        "red;"
+      elsif current > maximum - (maximum * 0.15)
+        "orange;"
+      else
+        "green;"
+      end
+   end
+
 end
+
